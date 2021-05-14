757a62b4bc7b304ed9ddb4c23941f871d01e9c76
sumitmokal <59595603+sumitmokal@users.noreply.github.com>
2021-05-14 13:08:45 +0530

    Update aws_ec2.tf

--- aws_ec2.tf
+++ aws_ec2.tf
@@ -2,8 +2,6 @@ resource "aws_instance" "aws-cloud-automation-meetup-ec2" {
   ami = "ami-0a887e401f7654935"
   instance_type = "t2.micro"
 
-  key_name = "cfn-key-1"
-
   user_data = file("install.sh")
 
   tags = {
