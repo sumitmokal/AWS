2bedbff887437efdaec1a1913b0015b6da44de93
sumitmokal <59595603+sumitmokal@users.noreply.github.com>
2021-05-14 13:00:29 +0530

    Delete install.sh

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo yum update -y
-sudo yum install httpd -y
-sudo service httpd start
-sudo chkconfig httpd on
-echo "<h1>Deployed via AWS Cloud Automation</h1>" | sudo tee /var/www/html/index.html
